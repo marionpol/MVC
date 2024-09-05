d289c7a4dfc328fc846c32ca6d2ad4c08edda3ff
Marionpol <marion.poljakov@gmail.com>
2024-09-05 15:43:57 +0300

    Display all articles using the article db model

---
+++ models/article.js
@@ -0,0 +1,16 @@
+const BaseSQLModel = require('./base');
+
+// Create a new class for a specific table
+class ArticleModel extends BaseSQLModel {
+  constructor() {
+    super('article');
+  }
+
+  async findAll(){
+    const articles = await super.findAll();
+    return articles;
+  }
+}
+
+module.exports = ArticleModel;
+
