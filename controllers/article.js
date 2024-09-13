2996e9f3d8d1bee6619807e937d130683dc100c1
Marion Poljakov <marion.poljakov@gmail.com>
2024-09-13 11:30:51 +0300

    Fixes

--- controllers/article.js
+++ controllers/article.js
@@ -8,7 +8,7 @@ class articleController {
     
     async getAllArticles(req, res){
         const articles = await articleModel.findAll()
-        res.status(201).json({articles: articles})
+        res.status(201).render('index', {articles: articles})
     }
 
     async getArticleBySlug(req,res){
